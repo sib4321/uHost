18da81896eea3d437ca6ade5c461d49a9e80226e
sib4321 <sib4321@ukr.net>
2018-04-09 11:41:23 +0300

    Add transforms, transitions and animations

--- shared.js
+++ shared.js
@@ -4,6 +4,7 @@ var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
+var ctaButton = document.querySelector(".main-nav__item--cta");
 
 // console.dir(backdrop.style['background-image']);
 
@@ -14,7 +15,10 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
     // backdrop.style.display = "block";
     // modal.className = 'open'; // This will actually overwrite the complete class list
     modal.classList.add("open");
-    backdrop.classList.add("open");
+    backdrop.style.display = "block";
+    setTimeout(function() {
+      backdrop.classList.add("open");
+    }, 10);
   });
 }
 
@@ -35,11 +39,29 @@ function closeModal() {
     modal.classList.remove("open");
   }
   backdrop.classList.remove("open");
+  setTimeout(function() {
+    backdrop.style.display = "none";
+  }, 200);
 }
 
 toggleButton.addEventListener("click", function() {
   // mobileNav.style.display = 'block';
   // backdrop.style.display = 'block';
   mobileNav.classList.add("open");
-  backdrop.classList.add("open");
+  backdrop.style.display = "block";
+  setTimeout(function() {
+    backdrop.classList.add("open");
+  }, 10);
 });
+
+ctaButton.addEventListener('animationstart', function (event) {
+  console.log('Animation started', event);
+})
+
+ctaButton.addEventListener('animationend', function (event) {
+  console.log('Animation ended', event);
+})
+
+ctaButton.addEventListener('animationiteration', function (event) {
+  console.log('Animation iteration', event);
+})
